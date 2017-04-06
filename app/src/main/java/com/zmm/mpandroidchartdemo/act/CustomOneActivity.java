553adbbaa9ff7f150101e99e31f56aa80e3c0f4a
Giousa <65489469@qq.com>
2017-04-06 09:44:14 +0800

    标题和icon的设置

--- app/src/main/java/com/zmm/mpandroidchartdemo/act/CustomOneActivity.java
+++ app/src/main/java/com/zmm/mpandroidchartdemo/act/CustomOneActivity.java
@@ -5,6 +5,10 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.zmm.mpandroidchartdemo.R;
+import com.zmm.mpandroidchartdemo.view.CustomLineView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Description:
@@ -15,9 +19,20 @@ import com.zmm.mpandroidchartdemo.R;
 
 public class CustomOneActivity extends AppCompatActivity {
 
+    @InjectView(R.id.custom_line_view)
+    CustomLineView mCustomLineView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_one);
+        ButterKnife.inject(this);
+        initView();
+    }
+
+    private void initView() {
+        mCustomLineView.setTitle("这个是一个标题");
+        mCustomLineView.setUnit("角度");
+        mCustomLineView.setLeftPic(R.drawable.bg_view_left_pic);
     }
 }
--- app/src/main/java/com/zmm/mpandroidchartdemo/view/CustomLineView.java
+++ app/src/main/java/com/zmm/mpandroidchartdemo/view/CustomLineView.java
@@ -1,21 +1,30 @@
 package com.zmm.mpandroidchartdemo.view;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
 import com.zmm.mpandroidchartdemo.R;
 
 /**
  * Description:
  * Author:zhangmengmeng
  * Date:2017/4/5
- * Time:下午5:41
+ * Time:下午5:41a
  */
 
 public class CustomLineView extends RelativeLayout {
-    
+
+    private TextView mChartTitle;
+    private TextView mChartUnit;
+    private ImageView mChartLeft;
+    private LineChart mChart;
+
     public CustomLineView(Context context) {
         this(context,null);
     }
@@ -27,10 +36,30 @@ public class CustomLineView extends RelativeLayout {
     public CustomLineView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         initView();
+        initData();
     }
 
     private void initView() {
         View view = View.inflate(getContext(), R.layout.custom_lineview, this);
+        mChartTitle = (TextView) view.findViewById(R.id.chart_tv_title);
+        mChartUnit = (TextView) view.findViewById(R.id.chart_tv_unit);
+        mChartLeft = (ImageView) view.findViewById(R.id.chart_iv_left_pic);
+        mChart = (LineChart) view.findViewById(R.id.chart_line_custom);
+    }
 
+    private void initData() {
+
+    }
+
+    public void setTitle(String title) {
+        mChartTitle.setText(title);
+    }
+
+    public void setUnit(String unit) {
+        mChartUnit.setText(unit);
+    }
+
+    public void setLeftPic(int leftPic) {
+        mChartLeft.setImageDrawable(getResources().getDrawable(leftPic));
     }
 }
