a6bd7640926dd6a2427ce6bf0816512e41ff7c96
Giousa <65489469@qq.com>
2017-04-06 11:51:49 +0800

    charjson

--- app/src/main/java/com/zmm/mpandroidchartdemo/act/FastJsonActivity.java
+++ app/src/main/java/com/zmm/mpandroidchartdemo/act/FastJsonActivity.java
@@ -10,6 +10,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.zmm.mpandroidchartdemo.R;
+import com.zmm.mpandroidchartdemo.utils.ChartJson;
 import com.zmm.mpandroidchartdemo.utils.TestUrl;
 
 import java.io.IOException;
@@ -32,115 +33,7 @@ public class FastJsonActivity extends AppCompatActivity {
 
     private static final String TAG = FastJsonActivity.class.getSimpleName();
 
-    String jsonUrl = "{\n" +
-            "    \"line1\": [\n" +
-            "        20,\n" +
-            "        23,\n" +
-            "        56,\n" +
-            "        8,\n" +
-            "        56,\n" +
-            "        23,\n" +
-            "        89,\n" +
-            "        90,\n" +
-            "        12,\n" +
-            "        45,\n" +
-            "        89,\n" +
-            "        1,\n" +
-            "        4,\n" +
-            "        8,\n" +
-            "        90,\n" +
-            "        45,\n" +
-            "        23,\n" +
-            "        67,\n" +
-            "        90,\n" +
-            "        23,\n" +
-            "        45,\n" +
-            "        67,\n" +
-            "        89,\n" +
-            "        34,\n" +
-            "        67,\n" +
-            "        89,\n" +
-            "        23,\n" +
-            "        56,\n" +
-            "        89,\n" +
-            "        12,\n" +
-            "        34,\n" +
-            "        45,\n" +
-            "        67,\n" +
-            "        89,\n" +
-            "        34,\n" +
-            "        23,\n" +
-            "        56,\n" +
-            "        78,\n" +
-            "        23,\n" +
-            "        56,\n" +
-            "        34,\n" +
-            "        45,\n" +
-            "        67,\n" +
-            "        78,\n" +
-            "        13,\n" +
-            "        45,\n" +
-            "        75,\n" +
-            "        23,\n" +
-            "        12,\n" +
-            "        45,\n" +
-            "        89,\n" +
-            "        45,\n" +
-            "        23,\n" +
-            "        56,\n" +
-            "        78,\n" +
-            "        9\n" +
-            "    ],\n" +
-            "    \"line2\": [\n" +
-            "        12,\n" +
-            "        34,\n" +
-            "        45,\n" +
-            "        67,\n" +
-            "        86,\n" +
-            "        45,\n" +
-            "        23,\n" +
-            "        12,\n" +
-            "        34,\n" +
-            "        23,\n" +
-            "        56,\n" +
-            "        78,\n" +
-            "        45,\n" +
-            "        34,\n" +
-            "        12,\n" +
-            "        34,\n" +
-            "        7,\n" +
-            "        4,\n" +
-            "        12,\n" +
-            "        9,\n" +
-            "        2,\n" +
-            "        45,\n" +
-            "        67,\n" +
-            "        23,\n" +
-            "        12,\n" +
-            "        77,\n" +
-            "        23,\n" +
-            "        2,\n" +
-            "        34,\n" +
-            "        56,\n" +
-            "        34,\n" +
-            "        12,\n" +
-            "        34,\n" +
-            "        45,\n" +
-            "        67,\n" +
-            "        21,\n" +
-            "        34,\n" +
-            "        45,\n" +
-            "        56,\n" +
-            "        34,\n" +
-            "        45,\n" +
-            "        12,\n" +
-            "        34,\n" +
-            "        8,\n" +
-            "        3,\n" +
-            "        23,\n" +
-            "        45\n" +
-            "    ]\n" +
-            "}";
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -165,7 +58,7 @@ public class FastJsonActivity extends AppCompatActivity {
 
 
     private void jsonParse() {
-        JSONObject jsonObject = JSON.parseObject(jsonUrl);
+        JSONObject jsonObject = JSON.parseObject(ChartJson.CHARTJSON01);
         JSONArray line1 = jsonObject.getJSONArray("line1");
         Log.d(TAG,line1.toString());
         JSONArray line2 = jsonObject.getJSONArray("line2");
